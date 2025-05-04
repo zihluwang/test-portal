88627eb671000a8c47c9038a385b3eed7c545527
zihluwang <really@zihlu.wang>
2025-05-04 13:06:35 +0800

    feat: added PING API

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello world!" });
+  return NextResponse.json({ message: "Hello from Next.js API!" });
 }
