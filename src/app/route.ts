b69f70dcd1714114b101b045b19e9539762c0f3a
zihluwang <really@zihlu.wang>
2025-05-07 19:20:39 +0800

    style: reformat codes

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello from Next.js API!" });
+  return NextResponse.json({ message: "Hello from Next.js API!" })
 }
